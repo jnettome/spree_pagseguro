c778c23b92e88262cc51c75d59bde13621440e80
João Netto <hi@joaonetto.me>
2014-01-08 23:59:59 -0200

    Fixing variable name error

--- app/controllers/spree/pagseguro_controller.rb
+++ app/controllers/spree/pagseguro_controller.rb
@@ -4,7 +4,7 @@ module Spree
       @order = Spree::Order.find_by_number(params[:order])
       payment_method = Spree::PaymentMethod.where(type: 'Spree::BillingIntegration::Pagseguro::Checkout').first
 
-      pagseguro_transaction = PagseguroTransaction.find_by_order_id(order.id.to_s)
+      pagseguro_transaction = PagseguroTransaction.find_by_order_id(@order.id.to_s)
       pagseguro_transaction.update_attribute :status, 'waiting'
 
       payment = @order.payments.where(:state => "pending",
