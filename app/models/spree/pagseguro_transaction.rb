44b344b1683a9b77fd4f5192e80f1ae151e69be9
João Netto <hi@joaonetto.me>
2014-01-13 12:40:13 -0200

    Adding logs to notify process

--- app/models/spree/pagseguro_transaction.rb
+++ app/models/spree/pagseguro_transaction.rb
@@ -12,10 +12,8 @@ module Spree
       notification_code = params[:notificationCode]
       notification = Spree::BillingIntegration::Pagseguro::Checkout.notification(method.preferred_email, method.preferred_token, notification_code)
 
-      puts notification.inspect
-
-      pagseguro_transaction = self.find_by_order_id(notification.id)
-      pagseguro_transaction.params = params
+      pagseguro_transaction = self.find_by_order_id(Spree::Order.find_by_number(notification.reference).id.to_s)
+      pagseguro_transaction.params = params.to_s
       pagseguro_transaction.transaction_id = notification.transaction_id
       pagseguro_transaction.customer_id = notification_code # Todo refactor column name
       pagseguro_transaction.status = 'paid' if notification.approved?
